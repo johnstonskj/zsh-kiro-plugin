8ccfb530de8341ada1c4fb2467f39680b647b383
Simon Johnston <johnstonskj@gmail.com>
2026-03-13 07:06:49 -0700

    feat: update completion, add docs

--- kiro.plugin.zsh
+++ kiro.plugin.zsh
@@ -11,6 +11,6 @@ if [[ "${TERM_PROGRAM}" == "kiro" ]]; then
     if command -v kiro >/dev/null 2>&1; then
         source "$(kiro --locate-shell-integration-path zsh)"
     else
-        log_error "cannot initialize Kiro shell integrations, it's not on the path"
+        log_error "zsh-kiro: cannot initialize Kiro shell integrations, it's not on the path"
     fi
 fi
